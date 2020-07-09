3fe18ec541e4698450113b67287b60cffb5d5644
Hammad Saeed <hammadsaeed_1997@hotmail.com>
2020-07-09 16:48:29 +0800

    feat(client): working light change event

--- yeeweb/src/App.tsx
+++ yeeweb/src/App.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import button from './stories/1-Button.stories'
-import { changeColorReq, changeLight } from './components/requests'
+import { discoverLight, changeLight } from './components/requests'
 const url = 'http://localhost:8000'
 
-const sendRequest = (incomingReq: string) => {
-  // const sendReq = changeColorReq(incomingReq);
-  // console.log(sendReq);
+const sendColorChangeRequest = (incomingReq: string) => {
   changeLight(`${url}/changeLight`, { incomingReq })
   .then(data => {
-    console.log(data); // JSON data parsed by `data.json()` call
+    console.log(data);
   });
 }
+
+const sendDiscoverRequest = (incomingReq: string) => {
+  discoverLight(`${url}/discoverLight`, { incomingReq })
+  .then(data => {
+    console.log(data);
+  });
+}
+
 
 function App() {
   return (
@@ -22,7 +28,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <button onClick={() => sendRequest('Changing Color')} > fsf</button>
+        <button onClick={() => sendColorChangeRequest('Changing Color')} > Change</button>
+        <button onClick={() => sendDiscoverRequest('Discover')} > Discover</button>
         <a
           className="App-link"
           href="https://reactjs.org"
--- yeeweb/src/components/requests.ts
+++ yeeweb/src/components/requests.ts
@@ -24,14 +24,22 @@ export const changeColorReq = (incomingReq: string): any => {
   })
   .then(() => alert('Thank you for subscribing!'))
   .catch((error) => alert('ERRRR!'));
-  // .then(response => console.log(response.json()))
-  // .catch((error) => {console.log(error)})
-//     console.log(err)
-//   };
-//   // return getResponse
 }
 
 export async function changeLight(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+export async function discoverLight(url = '', data = {}) {
   // Default options are marked with *
   const response = await fetch(url, {
     method: 'POST',
